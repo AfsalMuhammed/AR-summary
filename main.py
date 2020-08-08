666036218abd00248ef8801c10031dfc07790198
Afsal Muhammed <afsal.muhd.m@gmail.com>
2020-08-08 18:59:49 +0530

    Added text detection using east

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import cv2
-# TODO detect text boxes inside a frame/image
+import utils.textdetection as textDetect
 
+image = cv2.imread("Images/13.jpg")
+
+# Saving a original image and shape
+
+
+orig = image.copy()
+# TODO detect text boxes inside a frame/image using EAST
+textBoxes = textDetect.image_processing(image, orig)
+
+print(textBoxes)
